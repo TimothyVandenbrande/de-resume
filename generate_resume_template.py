23eabb9a277b408944a4ad8921c85a63563da5a0
Timothy Vandenbrande <timothy.vandenbrande@gmail.com>
2018-04-09 22:47:22 +0200

    finx naming find

--- generate_resume_template.py
+++ generate_resume_template.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
-from docxtpl import DocxTemplate
+#from docxtpl import DocxTemplate
 import yaml
 import json
 import os
 
 def generate_it(i, f, t):
-    if ".yml" in f or ".yaml" in f:
+    if f.lower().endswith(('.yml', '.yaml')):
         context = yaml.load(open(i + "/" + f, 'r'))
-    elif ".json" in f:
+    elif f.lower().endswith('.json'):
         context = json.loads(open(i + "/" + f, 'r').read())
     doc.render(context)
     doc.save(i + "/" + f + ".docx")
-
 
 doc = DocxTemplate("de_cv_template.docx")
 
@@ -20,6 +19,6 @@ resumedir = os.getcwd() + '/resume'
 for r, d, f in os.walk(resumedir):
     for filename in f:
         truefile = os.path.join(r, filename)
-        if ".yml" in filename or ".yaml" in filename or ".json" in filename:
+        if filename.lower().endswith(('.yml', '.yaml', '.json')):
             generate_it(resumedir, filename, doc)
 
