1f6f9128d9e8a1b28d0df307a0e254b7d095d11c
Vandenbrande Timothy <timothyv@hi77028.cegekanv.corp.local>
2018-04-09 13:42:09 +0200

    cleanup

--- generate_resume_template.py
+++ generate_resume_template.py
@@ -2,10 +2,25 @@
 
 from docxtpl import DocxTemplate
 import yaml
+import json
+import os
+
+def generate_it(i, o, f, t):
+    if ".yml" in f or ".yaml" in f:
+        context = yaml.load(file(i + "/" + f, 'r'))
+    elif ".json" in f:
+        context = json.loads(file(i + "/" + f, 'r').read())
+    doc.render(context)
+    doc.save(o + "/" + f + ".docx")
 
 doc = DocxTemplate("de_cv_template.docx")
-stream = file('resume.yml', 'r')
-context = yaml.load(stream)
-print yaml.dump(context)
-doc.render(context)
-doc.save("generated_doc.docx")
+
+resumedir = os.getcwd() + '/resume'
+outputdir = os.getcwd() + '/generated'
+for r, d, f in os.walk(resumedir):
+    for filename in f:
+        truefile = os.path.join(r, filename)
+        print truefile
+        if ".yml" in filename or ".yaml" in filename or ".json" in filename:
+            generate_it(resumedir, outputdir, filename, doc)
+
