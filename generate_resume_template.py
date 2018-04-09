2947a54ea9680419edc9443ffc82f2508d96c79c
Timothy Vandenbrande <timothy.vandenbrande@gmail.com>
2018-04-09 23:11:11 +0200

    request van ThomasC

--- generate_resume_template.py
+++ generate_resume_template.py
@@ -5,20 +5,19 @@ import yaml
 import json
 import os
 
-def generate_it(i, f, t):
-    if f.lower().endswith(('.yml', '.yaml')):
-        context = yaml.load(open(i + "/" + f, 'r'))
-    elif f.lower().endswith('.json'):
-        context = json.loads(open(i + "/" + f, 'r').read())
-    doc.render(context)
-    doc.save(i + "/" + f + ".docx")
+def generate_it(resumedir, filename, template):
+    if filename.lower().endswith(('.yml', '.yaml')):
+        context = yaml.load(open(resumedir + "/" + filename, 'r'))
+    elif filename.lower().endswith('.json'):
+        context = json.loads(open(resumedir + "/" + filename, 'r').read())
+    template.render(context)
+    template.save(resumedir + "/" + filename + ".docx")
 
 doc = DocxTemplate("de_cv_template.docx")
 
 resumedir = os.getcwd() + '/resume'
-for r, d, f in os.walk(resumedir):
-    for filename in f:
+for r, d, filenames in os.walk(resumedir):
+    for filename in filenames:
         truefile = os.path.join(r, filename)
         if filename.lower().endswith(('.yml', '.yaml', '.json')):
             generate_it(resumedir, filename, doc)
-
