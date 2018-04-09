40a7cac665675761c80eb56df28aec3ba3c24980
Timothy Vandenbrande <timothy.vandenbrande@gmail.com>
2018-04-09 22:20:53 +0200

    Windows proof

--- generate_resume_template.py
+++ generate_resume_template.py
@@ -7,11 +7,12 @@ import os
 
 def generate_it(i, f, t):
     if ".yml" in f or ".yaml" in f:
-        context = yaml.load(file(i + "/" + f, 'r'))
+        context = yaml.load(open(i + "/" + f, 'r'))
     elif ".json" in f:
-        context = json.loads(file(i + "/" + f, 'r').read())
+        context = json.loads(open(i + "/" + f, 'r').read())
     doc.render(context)
     doc.save(i + "/" + f + ".docx")
+
 
 doc = DocxTemplate("de_cv_template.docx")
 
