486fd443287d3e81c6920936a35081536a185a35
Timothy Vandenbrande <timothy.vandenbrande@cegeka.com>
2019-04-24 15:27:04 +0200

    multi resume fix + add resume/* to gitignore

--- generate_resume_template.py
+++ generate_resume_template.py
@@ -5,23 +5,22 @@ import yaml
 import json
 import os
 
-def generate_cv(resumedir, outputdir, filename, template):
+def generate_cv(resumedir, outputdir, filename, templateLocation):
+    template = DocxTemplate(template)
     if filename.lower().endswith(('.yml', '.yaml')):
         context = yaml.load(open(resumedir + "/" + filename, 'r'), Loader=yaml.SafeLoader)
     elif filename.lower().endswith('.json'):
         context = json.loads(open(resumedir + "/" + filename, 'r').read())
     template.render(context)
-    print(filename)
     template.save(outputdir + "/" + os.path.splitext(filename)[0] + ".docx")
     context = None
 
 
-doc = DocxTemplate("templates/de_cv_template.docx.j2")
+templateLocation = "templates/de_cv_template.docx.j2"
 outputdir = os.getcwd() + "/output"
 resumedir = os.getcwd() + "/resume"
 
 for r, d, filenames in os.walk(resumedir):
     for filename in filenames:
-        truefile = os.path.join(r, filename)
         if filename.lower().endswith(('.yml', '.yaml', '.json')):
-            generate_cv(resumedir, outputdir, filename, doc)
+            generate_cv(resumedir, outputdir, filename, templateLocation)
