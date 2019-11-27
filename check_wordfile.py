25f9403b785ce3cfcbb1e43ff1c74cb9b62c7c35
Timothy Vandenbrande <timothy.vandenbrande@gmail.com>
2019-11-27 21:19:38 +0100

    updated the tests + added Markdown functionality

--- check_wordfile.py
+++ check_wordfile.py
@@ -1,12 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from docx import Document
-
 import textract
+
 text = textract.process('output/default-resume.docx', extension='docx')
 
+faulty = False
 for line in str(text).split('\n'):
     if "{{" in line or "}}" in line:
-        raise "Not all template variables have been resolved"
-    #print line
+        faulty = True
+        print(line)
 
+if faulty:
+    print("Not all template variables have been resolved")
+    raise ValueError
+
+filepath = 'output/default-resume.md'
+with open(filepath) as fp:
+    for cnt, line in enumerate(fp):
+        if "{{" in line or "}}" in line:
+            faulty = True
+            print(line)
+
+if faulty:
+    print("Not all template variables have been resolved")
+    raise ValueError
--- generate_resume_template.py
+++ generate_resume_template.py
@@ -1,26 +1,44 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from docxtpl import DocxTemplate
+from jinja2 import Environment, FileSystemLoader
 import yaml
 import json
 import os
 
-def generate_cv(resumedir, outputdir, filename, templateLocation):
-    template = DocxTemplate(templateLocation)
-    if filename.lower().endswith(('.yml', '.yaml')):
-        context = yaml.load(open(resumedir + "/" + filename, 'r'), Loader=yaml.SafeLoader)
-    elif filename.lower().endswith('.json'):
-        context = json.loads(open(resumedir + "/" + filename, 'r').read())
+def get_context(resumedir, filename):
+    if os.path.splitext(filename)[1] in ['.yml', '.yaml']:
+        context = yaml.load(open(resumedir + "/" + filename, 'r', encoding="utf-8"), Loader=yaml.SafeLoader)
+    elif os.path.splitext(filename)[1] in ['.json']:
+        context = json.loads(open(resumedir + "/" + filename, 'r', encoding="utf-8").read())
+    else:
+        print(os.path.splitext(filename)[1] + " is not a valid source file.")
+        context = {}
+    # print(yaml.dump(context, encoding=('utf-8')))
+    return context
+
+def generate_md(outputdir, context, md):
+    env = Environment(loader = FileSystemLoader(os.path.dirname(os.path.abspath(__file__))), trim_blocks=True)
+    template = env.get_template(md)
+    with open(outputdir + "/" + os.path.splitext(filename)[0] + ".md", 'w+', encoding="utf-8") as x_file:
+        rendered_template = template.render(context)
+        x_file.write(rendered_template)
+
+def generate_docx(outputdir, context, docx):
+    template = DocxTemplate(docx)
     template.render(context)
     template.save(outputdir + "/" + os.path.splitext(filename)[0] + ".docx")
-    context = None
 
-
-templateLocation = "templates/de_cv_template.docx.j2"
+docx = "templates/de_cv_template.docx.j2"
+md = "templates/de_cv_template.md.j2"
 outputdir = os.getcwd() + "/output"
 resumedir = os.getcwd() + "/resume"
 
 for r, d, filenames in os.walk(resumedir):
     for filename in filenames:
-        if filename.lower().endswith(('.yml', '.yaml', '.json')):
-            generate_cv(resumedir, outputdir, filename, templateLocation)
+        print(filename)
+        context = get_context(resumedir, filename)
+        if context != {}:
+            generate_md(outputdir, context, md)
+            generate_docx(outputdir, context, docx)
